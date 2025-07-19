c5119f2ff613a4f449282a1358ab93687d9f19c9
baseballclub <abkimdoyo25@gmail.com>
2025-07-19 20:07:36 +0900

    [Fix] 모델용 데이터 조회 함수 DataFrame 형태로 변환

--- services/report/data.py
+++ services/report/data.py
@@ -4,11 +4,25 @@ from datetime import date
 from typing import Optional
 
 from models.record import Record
+import pandas as pd
 
-# 모델 학습용 유저별/날짜별 소비내역 조회 함수
-def data_read(db: Session, user_id: int, spend_date: Optional[date]):
+# ML 모델 학습용 / 예측용 데이터 로드 함수, DataFrame 형태로 반환
+def data_read(db: Session, user_id: int, spend_date: Optional[str] = None) -> pd.DataFrame:
+   
     query = db.query(Record).filter(Record.userId == user_id)
-    if spend_date is not None:
+    if spend_date:
         query = query.filter(func.date(Record.spendDate) == spend_date)
-    result = query.all()
-    return result
+
+    records = query.all()
+
+    if not records:
+        return pd.DataFrame()
+
+    data = [{
+        'userId': r.userId,
+        'spendCost': r.spendCost,
+        'spendDate': r.spendDate
+    } for r in records]
+
+    df = pd.DataFrame(data)
+    return df
