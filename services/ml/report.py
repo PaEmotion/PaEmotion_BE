bcb6336c73cb2ce8659fbac13a4baf3a86bdf3a3
baseballclub <abkimdoyo25@gmail.com>
2025-07-19 18:54:33 +0900

    [Fix] 저장된 모델을 사용

--- services/ml/report.py
+++ services/ml/report.py
@@ -1,18 +1,16 @@
 from ai.utils import make_sliding_window_multi
 from services.report.data import data_read
 from ai import budgetTraining, budgetPrediction
+import joblib
 
 def training_and_prediction (userId:int):
     # 1. 데이터 조회하기
     df = data_read(userId)
 
     # 2. 데이터 전처리하기
-    X, y = make_sliding_window_multi(df)
+    X, _ = make_sliding_window_multi(df)
 
-    # 3. 모델 학습하기
-    model = budgetTraining(X,y)
-
-    # 4. 예측하기
+    # 3. 예측하기
+    model = joblib.load("ai/rf_model.pkl")
     y_pred = budgetPrediction(model, X[-1])
-
     return y_pred
