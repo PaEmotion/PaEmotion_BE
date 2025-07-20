2ee73ac4eb83f4e8a2a12e62bd4f6ac4637de462
baseballclub <abkimdoyo25@gmail.com>
2025-07-21 00:44:24 +0900

    [Refactor] 데이터 조회 관련 함수 삭제로 인한 수정

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ from fastapi import FastAPI
 from routers.user.user import router as user_router 
 from routers.user.email import router as email_router
 from routers.record import router as record_router
-from routers.report.data import router as report_data_router
 from routers.report.report import router as report_router
-from routers.ml.report import router as budget_router
+from routers.ml.budget import router as budget_router
+from routers.ml.type import router as type_router
+
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(email_router)
 app.include_router(record_router) 
-app.include_router(report_data_router)
 app.include_router(report_router)
 app.include_router(budget_router)
+app.include_router(type_router)
 
 @app.get("/")
 def read_root():
