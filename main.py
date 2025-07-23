278d78596c2d979add692b535c7c3a0153bff7a8
baseballclub <abkimdoyo25@gmail.com>
2025-07-24 03:19:33 +0900

    [Faet] GPT 레포트 라우터 등록

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from fastapi import FastAPI
 from routers.user.user import router as user_router 
 from routers.user.email import router as email_router
 from routers.record import router as record_router
-from routers.report.report import router as report_router
+from routers.report.reportRepo import router as report_router
 from routers.ml.budget import router as budget_router
 from routers.ml.type import router as type_router
+from routers.report.reportGPT import router as reportGPT_router
 
 app = FastAPI()
 
@@ -19,6 +20,7 @@ app.include_router(record_router)
 app.include_router(report_router)
 app.include_router(budget_router)
 app.include_router(type_router)
+app.include_router(reportGPT_router)
 
 @app.get("/")
 def read_root():
