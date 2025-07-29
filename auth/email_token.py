bca6becdc02174c4da631ebe4f9eb4532866fdd9
baseballclub <abkimdoyo25@gmail.com>
2025-07-29 20:51:54 +0900

    [Refatcor] dependencies.py 관련 리팩토링

--- auth/email_token.py
+++ auth/email_token.py
@@ -1,10 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from jose import jwt, JWTError
-import os
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
-ALGORITHM = "HS256"
-EMAIL_TOKEN_EXPIRE_MINUTES = int(os.environ.get("EMAIL_TOKEN_EXPIRE_MINUTES"))
+from auth.dependencies import SECRET_KEY, ALGORITHM, EMAIL_TOKEN_EXPIRE_MINUTES
 
 def create_email_token(email: str) -> str:
     expire = datetime.now(timezone.utc) + timedelta(minutes=EMAIL_TOKEN_EXPIRE_MINUTES)
