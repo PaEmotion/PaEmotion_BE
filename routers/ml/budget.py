a0db18268e4e6d916ecea47f1d52c5216fe8331f
baseballclub <abkimdoyo25@gmail.com>
2025-08-10 18:33:04 +0900

    [Fix] 다음주 소비 금액 예측 라우터, test 라우터 분리 및 수정

--- routers/ml/budget.py
+++ routers/ml/budget.py
@@ -1,20 +1,41 @@
 from fastapi import APIRouter, Depends
-from services.ml.budget import training_and_prediction
+from services.ml.budget import training_and_prediction, training_and_prediction_test
 from sqlalchemy.orm import Session
 from db.session import get_db
 from utils.response import response_success
 from models.user import User
 from auth.jwt_token import get_current_user
+from numpy import expm1
 router = APIRouter(prefix="/ml")
 
 @router.get("/predict")
 def budgetPrediction(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
-    y_pred = training_and_prediction(db, current_user.userId)
+    preds = training_and_prediction(db, current_user.userId)
 
-    if not y_pred:
+    if preds is None:
         return response_success(data={}, message="예측할 데이터가 부족합니다.")
 
+    real_pred = expm1(float(preds))
+    formatted_pred = f"{round(real_pred):,}원"
+
     return response_success(
-        data = {"예측" : y_pred},
+        data={"예측": formatted_pred},
+        message="예측 성공"
+    )
+
+@router.get("/predict/test")
+def budgetPredictionTest(current_user: User = Depends(get_current_user), db:Session = Depends(get_db)):
+    preds, mae = training_and_prediction_test(db, current_user.userId)
+
+    if not preds:
+        return response_success(data={}, message="예측할 데이터가 부족합니다.")
+
+    real_preds = expm1(preds)
+    formatted_preds = f"{round(real_preds):,}원"
+    return response_success(
+        data = {
+            "예측" : formatted_preds,
+            "평가" : mae
+            },
         message = "예측"
     )
