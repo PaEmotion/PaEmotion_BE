34bf85259b8688cb6a73083e94bc9da5b8438295
baseballclub <abkimdoyo25@gmail.com>
2025-09-02 10:17:00 +0900

    [Fix] 순환 참조 수정

--- db/session.py
+++ db/session.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-from auth.dependencies import DATABASE_URL
 load_dotenv()
 
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 def get_db():
+    from auth.dependencies import DATABASE_URL
+    engine = create_engine(DATABASE_URL, echo=True)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     db = SessionLocal()
     try:
         yield db
